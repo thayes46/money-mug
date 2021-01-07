307cb99b9c81c701eeca643cf264b2e415d53b11
christiandow <68239276+christiandow@users.noreply.github.com>
2021-01-07 16:57:02 -0500

    Update parameter_passer.py

--- parameter_passer.py
+++ parameter_passer.py
@@ -5,7 +5,7 @@ import boto3
 from botocore.exceptions import ClientError
 
 
-def jsonCreation(name, limit, range):
+def jsonCreation(name, limit, range): # creates json for all the information given by the user.
     jsonObj = {} #creation of JSON object
     jsonObj['data'] = []
     jsonObj['data'].append({'name': name, 'limit': limit, 'range': range})
@@ -13,6 +13,12 @@ def jsonCreation(name, limit, range):
     return jsonObj
 
 def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
     if object_name is None:
         object_name = file_name
 
@@ -26,7 +32,7 @@ def upload_file(file_name, bucket, object_name=None):
         return False
     return True
 
-name = input('Name: ')
+name = input('Name: ') #these inputs would come straight from the app
 limit = input('Max Spending Limit: ')
 range = input('Over what time range: ')
 
