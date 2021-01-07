a938b9fa900c2ffb29fcac7d2ca0c9daf0f9abc9
Naimul Hasan <naimulh247@gmail.com>
2021-01-07 12:22:25 -0500

    Updated Serial Communication btw Python & Arduino

---
+++ arduino/ArduinoToPCSerial.ino
@@ -0,0 +1,173 @@
+
+#include <Adafruit_NeoPixel.h>
+
+#define PIN        6
+#define DELAYVAL 500
+#define NUMPIXELS 10
+
+
+const byte buffSize = 40;
+char inputBuffer[buffSize];
+const char startMarker = '<';
+const char endMarker = '>';
+byte bytesRecvd = 0;
+boolean readInProgress = false;
+boolean newDataFromPC = false;
+
+char messageFromPC[buffSize] = {0};
+int red = 0;
+int green = 0;
+int blue = 0;
+
+unsigned long curMillis;
+
+unsigned long prevReplyToPCmillis = 0;
+unsigned long replyToPCinterval = 1000;
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+//=============
+
+void setup() {
+  Serial.begin(9600);
+  pixels.begin();
+    // flash LEDs so we know we are alive
+  for (byte n = 0; n < NUMPIXELS; n++) {
+     pixels.setPixelColor(n, pixels.Color(0, 100, 100));
+     pixels.show();
+     delay(DELAYVAL);
+  }
+  delay(500); // delay() is OK in setup as it only happens once
+  
+  for (byte n = 0; n < NUMPIXELS; n++) {
+     pixels.setPixelColor(n, pixels.Color(0, 0, 100));
+     pixels.show();
+     delay(DELAYVAL);
+  }
+  
+  pixels.clear();
+    // tell the PC we are ready
+  Serial.println("<Arduino is ready>");
+}
+
+//=============
+
+void loop() {
+  curMillis = millis();
+  getDataFromPC();
+  updateNeoPixelColors();
+  replyToPC();
+//  lightUp();
+
+}
+
+//=============
+
+void getDataFromPC() {
+
+    // receive data from PC and save it into inputBuffer
+    
+  if(Serial.available() > 0) {
+
+    char x = Serial.read();
+
+      // the order of these IF clauses is significant
+      
+    if (x == endMarker) {
+      readInProgress = false;
+      newDataFromPC = true;
+      inputBuffer[bytesRecvd] = 0;
+      parseData();
+    }
+    
+    if(readInProgress) {
+      inputBuffer[bytesRecvd] = x;
+      bytesRecvd ++;
+      if (bytesRecvd == buffSize) {
+        bytesRecvd = buffSize - 1;
+      }
+    }
+
+    if (x == startMarker) { 
+      bytesRecvd = 0; 
+      readInProgress = true;
+    }
+  }
+}
+
+//=============
+ 
+void parseData() {
+
+    // split the data into its parts
+    
+  char * strtokIndx; // this is used by strtok() as an index
+  
+  strtokIndx = strtok(inputBuffer,",");      // get the first part - the string
+  strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+  
+  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+  red = atoi(strtokIndx);     // convert this part to an integer
+  
+  strtokIndx = strtok(NULL, ","); 
+  green = atoi(strtokIndx);     // convert this part to a integer
+
+  strtokIndx = strtok(NULL, ",");
+  blue = atoi(strtokIndx);
+  
+
+}
+
+//=============
+
+void replyToPC() {
+
+  if (newDataFromPC) {
+    newDataFromPC = false;
+    Serial.print("<Msg ");
+    Serial.print(messageFromPC);
+    Serial.print(" Red ");
+    Serial.print(red);
+    Serial.print(" Green ");
+    Serial.print(green);
+    Serial.print(" Green ");
+    Serial.print(blue);
+    Serial.print(" Blue ");
+
+    Serial.print(" Time ");
+    Serial.print(curMillis >> 9); // divide by 512 is approx = half-seconds
+    Serial.println(">");
+  }
+}
+
+//============
+
+void updateNeoPixelColors() {
+
+   // this illustrates using different inputs to call different functions
+  if (strcmp(messageFromPC, "update") == 0) {
+     updateNeo();
+  }
+  
+  
+}
+
+//=============
+
+void updateNeo() {
+
+  for (byte n = 0; n < NUMPIXELS; n++) {
+     pixels.setPixelColor(n, pixels.Color(red, green, blue));
+     pixels.show();
+     delay(DELAYVAL);
+  }
+}
+
+//=============
+
+//void
+
+//=============
+
+
+//=============
+
--- sendColor.py
+++ sendColor.py
@@ -1,11 +1,119 @@
+
+#=====================================
+
+#  Function Definitions
+
+#=====================================
+
+def sendToArduino(sendStr):
+    serialConnection.write(sendStr.encode('utf-8'))
+
+
+#======================================
+
+def recvFromArduino():
+    global startMarker, endMarker
+    
+    ck = ""
+    x = "z" # any value that is not an end- or startMarker
+    byteCount = -1 # to allow for the fact that the last increment will be one too many
+    
+    # wait for the start character
+    while  ord(x) != startMarker: 
+        x = serialConnection.read()
+    
+    # save data until the end marker is found
+    while ord(x) != endMarker:
+        if ord(x) != startMarker:
+            ck = ck + x.decode("utf-8") # change for Python3
+            byteCount += 1
+        x = serialConnection.read()
+    
+    return(ck)
+
+
+#============================
+
+def waitForArduino():
+
+    # wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
+    
+    global startMarker, endMarker
+    
+    msg = ""
+    while msg.find("Arduino is ready") == -1:
+
+        while serialConnection.inWaiting() == 0:
+            pass
+        
+        msg = recvFromArduino()
+
+        print(msg) 
+        print()
+        
+#======================================
+
+def run(td):
+    numLoops = len(td)
+    waitingForReply = False
+
+    n = 0
+    while n < numLoops:
+        teststr = td[n]
+
+        if waitingForReply == False:
+            sendToArduino(teststr)
+            print("Sent from PC -- LOOP NUM " + str(n) + " STR Sent " + teststr)
+            waitingForReply = True
+
+        if waitingForReply == True:
+
+            while serialConnection.inWaiting() == 0:
+                pass
+            
+            dataRecvd = recvFromArduino()
+            print("Reply Received  " + dataRecvd)
+            n += 1
+            waitingForReply = False
+
+            print("===========")
+
+        time.sleep(5)
+
+
+#======================================
+
+# The Program Starts Here
+
+#======================================
+
 import serial
+import time
+
+print()
+print()
+
+# NOTE the user must ensure that the serial port and baudrate are correct
+# serPort = "/dev/ttyS80 or /dev/cu.usbserial-0001"
+serPort = "/dev/cu.usbserial-0001"
+baudRate = 9600
+serialConnection = serial.Serial(serPort, baudRate)
+print("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
 
 
-# ask for the arduino port
-arduinoPort = input("Enter your board serial port (e.g. COM1 or /dev/cu.usbserial-0001)")
+startMarker = 60
+endMarker = 62
 
-serialConnection = serial.Serial(arduinoPort, 9600)
 
-while True:
-    inputValue = "" # this needs to be an api call, change later when we have an actual working api
-    serialConnection.write(inputValue.encode())
+waitForArduino()
+
+
+testData = []
+testData.append("<update,200,0,0>")
+
+
+run(testData)
+
+
+serialConnection.close
+
