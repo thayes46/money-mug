0e4636501e8ddb33790dd8e8c018ca7b7a723db6
Anthony <54818710+antbravo121@users.noreply.github.com>
2021-01-08 02:02:50 -0500

    Add files via upload

---
+++ MoneyMugWebApp/parameterpasser.py
@@ -0,0 +1,46 @@
+import json
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+
+def jsonCreation(name, limit, range): # creates json for all the information given by the user.
+    jsonObj = {} #creation of JSON object
+    jsonObj['data'] = []
+    jsonObj['data'].append({'name': name, 'limit': limit, 'range': range, 'category': 'coffee'})
+
+    return jsonObj
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+def main(userName, spending, timerange):
+    name = userName #these inputs would come straight from the app
+    limit = spending
+    lrange = timerange
+
+
+    jsonObj = jsonCreation(name, limit, lrange)
+
+    with open('newFile.json', 'w') as jsonFile:
+        json.dump(jsonObj, jsonFile)
+
+
+    upload_file('newFile.json', 'money-mug', )
